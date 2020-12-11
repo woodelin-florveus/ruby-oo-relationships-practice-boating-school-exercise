fbe1ac028aac4d5a1b165d24d622d75e04c54e53
woodelin-florveus <woodelinflorveus@gmail.com>
2020-12-11 15:15:18 -0500

    commit tree 930a8930d3a50c732c3d4b72ca6ca6f44f8528cb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,20 @@
 class Student
 
+    attr_reader :first_name
+
+    @@all = []
+
+def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def add_boating_test
+    
+end
+
 end
